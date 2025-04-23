d2cf9268e3824f99c74239d582fd0d4f11560721
fortunewalla <fortunewalla@users.noreply.github.com>
2025-04-23 16:42:07 +0530

    Update index.php to add phpinfo

--- app/index.php
+++ app/index.php
@@ -1,2 +1,12 @@
-<?php print("Hello 👋, from Wasmer Edge!"); ?>
+<?php print("Hello 👋, from Wasmer Edge!"); 
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+// phpinfo(INFO_MODULES);
+
+
+?>
 
